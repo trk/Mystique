46c72dca84a41e2b20fba30eb5b8672ab61e60fc
Iskender TOTOGLU <totoglu@gmail.com>
2021-02-20 21:17:43 +0300

    update readme and add new usage example

---
+++ configs/Mystique.magician.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace ProcessWire;
+
+/**
+ * Resource: magic of mystique field
+ */
+return function ($page = null, $field = null) {
+
+    $fields = [
+        'hello' => [
+            'label' => 'Are you ready for a Magic ?',
+            'type' => 'select',
+            'options' => [
+                'no' => 'No',
+                'yes' => 'Yes'
+            ],
+            'required' => true,
+            'defaultValue' => 'no'
+        ]
+    ];
+
+    if ($page instanceof Page && $page->template->name == 'page') {
+        $fields['current_page'] = [
+            'label' => 'Current page title : ' . $page->title,
+            'value' => $page->title,
+            'showIf' => [
+                'hello' => '=yes'
+            ],
+            'columnWidth' => 50
+        ];
+    }
+
+    if ($field instanceof Field) {
+        $fields['current_field'] = [
+            'label' => 'Current field label : ' . $field->label,
+            'value' => $field->label,
+            'showIf' => [
+                'hello' => '=yes'
+            ],
+            'columnWidth' => 50
+        ];
+    }
+
+    return [
+        'name' => 'magician',
+        'title' => 'Do A Magic ?',
+        'fields' => $fields
+    ];
+
+};
