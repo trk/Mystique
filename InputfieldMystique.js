38d12271784b2f28971aeea91c361bb0ebf96953
Iskender TOTOGLU <totoglu@gmail.com>
2021-08-06 20:34:53 +0300

    remove javascript debug method

--- InputfieldMystique.js
+++ InputfieldMystique.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", function (event) {
     let inputFields = document.querySelectorAll('[data-mystique-export="1"]');
-    console.log(inputFields);
     inputFields.forEach(inputField => {
         console.log(inputField);
         inputField.addEventListener('click', function () {
