14b4fdab0fa2e093a847e4901a47aa1793ec83f3
Iskender TOTOGLU <totoglu@gmail.com>
2022-08-08 14:16:13 +0300

    update module version

--- Mystique.module.php
+++ Mystique.module.php
@@ -48,7 +48,7 @@ class Mystique extends WireData implements Module
     {
         return [
             'title' => 'Mystique',
-            'version' => '0.0.19',
+            'version' => '0.0.20',
             'summary' => __('Mystique is a config file based field creation module for ProcessWire CMS/CMF by ALTI VE BIR.'),
             'href' => 'https://www.altivebir.com',
             'author' => 'İskender TOTOĞLU | @ukyo(community), @trk (Github), https://www.altivebir.com',
